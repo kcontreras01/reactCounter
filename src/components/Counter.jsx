8aeba6d8ca5bdf9c354ee492091c546ae8ccc8c2
kcontreras01 <kcontreras01@gmail.com>
2019-11-25 18:57:40 -0500

    list rendering

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,6 +3,7 @@ import React, {Component} from 'react'
 class Counter extends Component {
 	state = {
 		count: 0,
+		tags: ['tag1','tag2','tag3']
 	}
 
 	formatCount() {
@@ -20,6 +21,9 @@ class Counter extends Component {
 			<React.Fragment>
 				<span className={this.getBadgeClasses()}>{this.formatCount()}</span>
 				<button className="btn btn-secondary btn-sm">Increment</button>
+				<ul> 
+					{ this.state.tags.map(tag => <li key={tag}>{tag}</li>) }
+				</ul>
 			</React.Fragment>
 			)
 	}
